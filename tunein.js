82d173cbb581643723033cc7838e465ba92da18f
Julien Ledun <j.ledun@iosystems.fr>
2016-11-11 18:24:07 +0100

    add search function in tunein repo browse needs a lot of completion and improvment
    Signed-off-by: Julien Ledun <j.ledun@iosystems.fr>

---
+++ tunein.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+
+module.exports = class tunein {
+
+  constructor() {
+    this.reset();
+  }
+
+  reset() {
+    this.url = {};
+    this.keys = [];
+    this.url.protocol = "http";
+    this.url.host = "opml.radiotime.com";
+    this.initSearch();
+  }
+
+  get() {
+    return new Promise( (resolve, reject) => {
+      let tmpurl = this.url;
+      tmpurl.search = querystring.stringify(this.url.search);
+      console.log( url.format(tmpurl) );
+      let req = http.get( url.format(tmpurl), (res) => {
+        let tuneinRes = "";
+        res.on('data', (chunk) => tuneinRes += chunk);
+        res.on('end', () => { 
+          let data = this.parseResult( JSON.parse( tuneinRes ) );
+          return resolve( ( data ) ); 
+        });
+      });
+      req.on('error', (err) => { return reject( err ); });
+    });
+  }
+
+  parseResult( data ) {
+    if ( data.head.status != 200 ) return new Error(`TuneIn Request error : ${data.head.fault}`);
+    switch (data.head.title) {
+      case "Browse":
+        data.body.forEach( (elm) => {
+          if ( this.keys.indexOf( elm.key ) === -1 ) this.keys.push( elm.key );
+        });
+        break;
+
+      default:
+        data.body.forEach( (elm) => {
+          if ( typeof elm.children != "undefined") elm.children = JSON.stringify(elm.children);
+        });
+        break;
+    }
+    return data;
+  }
+
+  getRoot() {
+    return this.elements;
+  }
+
+  initSearch() {
+    this.url.search = {render: "json"};
+  }
+
+  browse( category ) {
+    let cat = category || '';
+    this.url.pathname = "Browse.ashx";
+    this.initSearch();
+    if ( cat ) {
+      if ( this.keys.indexOf( cat ) !== -1 ) {
+        this.url.search.c = cat;
+      }else{
+        this.url.search.id = cat;
+      }
+    }
+    return this.get();
+  }
+
+  search( chunk ) {
+    this.url.pathname = "Search.ashx";
+    this.initSearch();
+    this.url.search.query = chunk;
+    return this.get();
+  }
+
+}
+
